697ce9781b4b220059670a505cd82db24335ce0f
choudharyamit26 <ravichoudhary766@gmail.com>
2021-06-24 12:21:39 +0530

    deploying to aws

--- brasi/urls.py
+++ brasi/urls.py
@@ -19,12 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('adminpanel/', include('adminpanel.urls', namespace='adminpanel')),
-    path('', include('userapp.urls', namespace='userapp')),
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('adminpanel/', include('adminpanel.urls', namespace='adminpanel')),
+                  path('', include('userapp.urls', namespace='userapp')),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
