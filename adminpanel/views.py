8dfd33ad3e7a22960d2abccb8dcdc10184231284
choudharyamit26 <ravichoudhary766@gmail.com>
2021-07-07 13:29:55 +0530

    added otp

--- adminpanel/views.py
+++ adminpanel/views.py
@@ -644,7 +644,7 @@ class SuperAdminBrasiSupscriptionView(LoginRequiredMixin, ListView):
     model = SubscriptionPlan
     template_name = 'superadmin/new/brasi-plan.html'
     login_url = "adminpanel:superadmin"
-    paginate_by = 10
+    paginate_by = 1
 
     def get(self, request, *args, **kwargs):
         subs_obj = None
