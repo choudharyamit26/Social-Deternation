b9fd199b894ceeb70cc27b2dbc8cf8e0dbc9a9d2
choudharyamit26 <ravichoudhary766@gmail.com>
2021-07-10 22:41:11 +0530

    completed service provider filter

--- brasi/settings.py
+++ brasi/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6eqr2i28gn8c6f&66%o9iw87i+j)y_$j!t#i5+2q!&25kuf979'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -75,25 +75,25 @@ WSGI_APPLICATION = 'brasi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 #### PRODUCTION DATABASE
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'brasidatabase',
-        'USER': 'brasi',
-        'PASSWORD': 'Brasi2021',
-        'HOST': 'database-1.ccpmpmdlsxmk.us-east-2.rds.amazonaws.com',
-        'PORT': '5432'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'brasidatabase',
+#         'USER': 'brasi',
+#         'PASSWORD': 'Brasi2021',
+#         'HOST': 'database-1.ccpmpmdlsxmk.us-east-2.rds.amazonaws.com',
+#         'PORT': '5432'
+#     }
+# }
 
 
 # Password validation
